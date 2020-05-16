2ffed39fc7033c1e56d52a7ea228080a3cbfcd8b
Deanna Earley <dee@earlsoft.co.uk>
2020-05-16 14:45:45 +0100

    Improved camera control with look up/down control

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -2,14 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraMover : MonoBehaviour
-{
+public class CameraMover : MonoBehaviour {
     public GameObject forwardRef = null;
 
-    void Update()
-    {
-        this.transform.position +=  this.forwardRef.transform.forward * Input.GetAxis("Vertical");
-        this.transform.position +=  this.forwardRef.transform.right * Input.GetAxis("Horizontal");
-        this.transform.Rotate(new Vector3(0,Input.GetAxis("Horizontal2") * 5, 0),Space.Self);
+    void Update () {
+        this.transform.position += this.forwardRef.transform.forward * Input.GetAxis ("Vertical");
+        this.transform.position += this.forwardRef.transform.right * Input.GetAxis ("Horizontal");
+        this.transform.Rotate (this.forwardRef.transform.right, -Input.GetAxis ("Vertical2") * 5, Space.World);
+        this.transform.Rotate (this.transform.up, Input.GetAxis ("Horizontal2") * 5, Space.World);
     }
 }
